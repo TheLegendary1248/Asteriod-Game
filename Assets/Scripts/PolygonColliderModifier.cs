dba4190743cc67db63f8c11aab53b84687c5e6b6
TheLegendary1248 <1248alex@gmail.com>
2023-04-28 17:40:28 -0400

    Asteroid Shader Finer Detail

--- Assets/Scripts/PolygonColliderModifier.cs
+++ Assets/Scripts/PolygonColliderModifier.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class PolygonColliderModifier
 {
     //General Function...
-    public static void Fracture()
+    public static void Fracture(this PolygonFill self)
     {
         throw new System.NotImplementedException();
         bool tooSmall = false;
@@ -13,18 +13,56 @@ public static class PolygonColliderModifier
         else { } //Fracture code
     }
     //Splits the asteroid along the given ray
-    public static void Split(Ray r)
+    public static void Split(this PolygonFill self, Ray r)
     {
+        PolygonCollider2D collider = self.polyCollider;
+        //Get line closest to ray origin
+        int startPoint = ClosestLine(collider.points, r.origin);
+        int endPoint = ClosestLine(collider.points, Vector2.zero);
+        //Get line closest to split exit point
 
+        //Generate half
+
+        //Generate fragment other half
     }
+    #region EXTRA
     //Dents the collider in said direction
-    public static void Dent(Ray r)
+    public static void Dent(this PolygonFill self, Ray r)
     {
 
     }
     //Splits the collider in several directions from a given center
-    public static void Crack(Vector2 center, Ray[] along)
+    public static void Crack(this PolygonFill self, Vector2 center, Ray[] along)
     {
 
+    }
+    #endregion
+    //Returns the lower index of the line pair that is closer to the point
+    public static int ClosestLine(Vector2[] shape, Vector2 point)
+    {
+        for (int i = 0; i < shape.Length; i++)
+        {
+            Vector2 start = shape[i];
+            Vector2 end = shape[i % (shape.Length - 1)];
+        }
+        return 0;
+    }
+    public static float SquarePointDistanceToLine(Vector2 point, Vector2 lineStart, Vector2 lineEnd)
+    {
+        float dividend = Mathf.Abs((lineEnd.x - lineStart.x) * (lineStart.y - point.y) - (lineStart.x - point.x) * (lineEnd.y - lineStart.y));
+        float divisor = Mathf.Sqrt(Mathf.Pow(lineEnd.x - lineStart.x, 2) + Mathf.Pow(lineEnd.y - lineStart.y, 2));
+
+        if (divisor != 0f)
+        {
+            return dividend / divisor;
+        }
+        else
+        {   // pointoints lineStart and lineEnd are the same, choose one
+            return Mathf.Pow(point.x - lineStart.x, 2) + Mathf.Pow(point.y - lineStart.y, 2);
+        }
+    }
+    public static GameObject GenerateFragment()
+    {
+        throw new System.NotImplementedException();
     }
 }
--- Assets/Scripts/PolygonFill.cs
+++ Assets/Scripts/PolygonFill.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 using UnityEditor;
 public class PolygonFill : MonoBehaviour
 {
-    public PolygonCollider2D col;
-    public MeshFilter mFil;
+    public PolygonCollider2D polyCollider;
+    public MeshFilter meshFilter;
     public LineRenderer outline;
 
     // Start is called before the first frame update
@@ -11,13 +11,13 @@ public class PolygonFill : MonoBehaviour
     {
 
         //Generate mesh
-        Mesh colMesh = col.CreateMesh(false, false);
+        Mesh colMesh = polyCollider.CreateMesh(false, false);
         //Fix Mesh UV
         colMesh.uv = System.Array.ConvertAll(colMesh.vertices, i => (Vector2)i);
-        mFil.mesh = colMesh;
+        meshFilter.mesh = colMesh;
         //Setup outline
-        outline.positionCount = col.points.Length;
-        outline.SetPositions(System.Array.ConvertAll(col.points, i => (Vector3)i));
+        outline.positionCount = polyCollider.points.Length;
+        outline.SetPositions(System.Array.ConvertAll(polyCollider.points, i => (Vector3)i));
     }
     
     // Update is called once per frame
