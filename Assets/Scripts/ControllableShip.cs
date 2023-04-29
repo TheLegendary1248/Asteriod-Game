b996955c64a2aff464ffc958f8cfb8894a21f57e
TheLegendary1248 <1248alex@gmail.com>
2023-04-29 02:20:08 -0400

    Trajectory Plot Function

--- Assets/Scripts/ControllableShip.cs
+++ Assets/Scripts/ControllableShip.cs
@@ -38,4 +38,5 @@ public class ControllableShip : MonoBehaviour
         }
         
     }
+    
 }
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public static class Utils
 {
-    
+    public static class Math
+    {   //Common math functions
+        public static Vector2[] PlotPoints()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static Vector2[] AcceleratedVelocity(Ray2D obj, Vector2 acceleration, int steps, float stepSize)
+        {
+            //Ensure conditions are correct
+            if (steps <= 0) throw new System.ArgumentOutOfRangeException("Steps cannot be less than one");
+            if (stepSize <= 0) throw new System.ArgumentOutOfRangeException("Step Size cannot be less than zero");
+            //Initialize
+            Vector2[] plot = new Vector2[steps];
+            Ray2D objAtPoint = obj;
+
+            //Step through and generate
+            for (int i = 0; i < steps; i++)
+            {
+                plot[i] = objAtPoint.origin;
+                objAtPoint.direction += acceleration * stepSize;
+                objAtPoint.origin += obj.direction * stepSize;
+            }
+            return plot;
+        }
+    }
 }
