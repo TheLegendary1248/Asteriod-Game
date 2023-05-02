fdb1564b28d122b2ecd3aa9cd1c4a1fee341f717
TheLegendary1248 <1248alex@gmail.com>
2023-05-02 01:31:27 -0400

    fixed camera

--- Assets/Scripts/CameraPanner.cs
+++ Assets/Scripts/CameraPanner.cs
@@ -12,9 +12,9 @@ public class CameraPanner : MonoBehaviour
         {
             focus = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
-        focus += new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Time.deltaTime * speed;
+        focus += new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Time.unscaledDeltaTime * speed;
         Camera.main.orthographicSize -= Input.mouseScrollDelta.y * (Camera.main.orthographicSize / 30);
-        transform.position = Vector3.Lerp(transform.position, (Vector3)focus + Vector3.back, lerp);
+        transform.position = Vector3.Lerp(transform.position, (Vector3)focus + Vector3.back, lerp * Time.unscaledDeltaTime);
     }
     public static Vector2 MousePos()
     {
