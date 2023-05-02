30518670519aa58ae53a4dde300f6a8855ae8f2a
TheLegendary1248 <1248alex@gmail.com>
2023-05-02 01:04:13 -0400

    Server Script, totally useless for now

---
+++ Assets/Scripts/Managers/ServerManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// I haven't started yet so I don't know
+/// </summary>
+public class ServerManager : MonoBehaviour
+{
+    
+}
